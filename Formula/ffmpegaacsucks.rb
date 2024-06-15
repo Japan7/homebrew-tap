3751773d1e64ee2c4c4bd616408d0feec73a3ca9
Nam Vu <git@yuru.moe>
2024-06-15 18:11:45 +0000

    ffmpegaacsucks: add 2.0.1 bottle.

--- Formula/ffmpegaacsucks.rb
+++ Formula/ffmpegaacsucks.rb
@@ -6,6 +6,13 @@ class Ffmpegaacsucks < Formula
   license "LGPL-3.0-or-later"
   head "https://github.com/odrling/ffmpegaacsucks.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/Japan7/homebrew-tap/releases/download/ffmpegaacsucks-2.0.1"
+    sha256 cellar: :any,                 arm64_sonoma: "649b6a02038bf5b0da9ef7ddc2096d01b9efe7490f4be5bc15d0c61278596352"
+    sha256 cellar: :any,                 ventura:      "f823a795320732ffd305df15540b0b80ba9df55e36ba69ebec9adeecfde56f06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "bc0bdcfa2d2bfb2ca85fc3cf901c0b255ca3116e58dd9f7cfd18832ae2cc202f"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
