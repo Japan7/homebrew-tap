de9c1c1dcc8958bee2a63267485b32fac55c5c12
Nam Vu <git@yuru.moe>
2024-06-15 18:11:45 +0000

    dakara_check: add 4.0.4 bottle.

--- Formula/dakara_check.rb
+++ Formula/dakara_check.rb
@@ -6,6 +6,13 @@ class DakaraCheck < Formula
   license "LGPL-3.0-or-later"
   head "https://github.com/Japan7/dakara_check.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/Japan7/homebrew-tap/releases/download/dakara_check-4.0.4"
+    sha256 cellar: :any,                 arm64_sonoma: "54f8c9ec1f858ae405b1d31b43beb039584ac5466ed235d695671d52c2456388"
+    sha256 cellar: :any,                 ventura:      "87c20ec791919db66fd49e16a2e791e63d5b3dac3c056ea1d039ea324f4d534c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e454836b9cf8a7f15d9cc0d3a9d705d5c101c6e737a6a480da5b7ed54bea41f5"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => [:build, :test]
